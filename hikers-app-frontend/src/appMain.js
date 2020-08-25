3ef0774e238915921025b291e50ea3330616c2db
LeonorPDX <leonoracolbert@gmail.com>
2020-08-25 15:51:39 -0700

    Added ID to trailhead JS objects, mostly finished render Trailheads function to create DOM nodes with correct formatting, removed divs from index.html so I can clear the 'main' node whenever the renderTrailheads function is called(otherwise, when you click 'Find Hikes' while already viewing trailheads, everything on the page gets duplicated every time 'Find Hikes' is clicked)

--- hikers-app-frontend/src/appMain.js
+++ hikers-app-frontend/src/appMain.js
@@ -14,18 +14,87 @@ class AppMain {
             .then(resp => resp.json())
             .then(data => {
                 data.forEach(th => {
-                    new Trailhead(th.name, th.location, th.amenities, th.fees, th.hikes)
+                    new Trailhead(th.name, th.location, th.amenities, th.fees, th.hikes, th.id)
                 })
             })
             .then(this.renderTrailheads)
             .catch(err => alert(err));
-        // populate trailheads property on appMain with instances
-        // Call the render function
+    }
+
+    getHikes() {
+        console.log("Get hikes button was clicked")
     }
 
     renderTrailheads() {
-        console.log("Rendering trailheads in the DOM...")
-        // Create the DOM nodes and insert JSON data in the DOM
+        const main = document.getElementById("main");
+        main.innerHTML = "";
+
+        const headline = document.createElement("div");
+        headline.className = "content";
+        const headlineText = document.createElement("h1");
+        headlineText.className = "title";
+        headlineText.innerText = "All Trailheads";
+        headline.appendChild(headlineText);
+        main.appendChild(headline);
+
+        AppMain.trailheads.forEach(th => {
+            const ancestor = document.createElement("div");
+            ancestor.className = "tile is-ancestor";
+
+            const parent = document.createElement("div");
+            parent.className = "tile is-parent";
+
+            const child = document.createElement("div");
+            child.className = "tile is-child box";
+
+            const level = document.createElement("div");
+            level.className = "level";
+
+            const left = document.createElement("div");
+            left.className = "level-left";
+
+            const leftItem = document.createElement("div");
+            leftItem.className = "level-item";
+
+            const title = document.createElement("p");
+            title.className = "title";
+            title.innerText = `${th.name}`;
+
+            leftItem.appendChild(title);
+            left.appendChild(leftItem);
+            level.appendChild(left);
+
+            if (th.hikes_count > 0) {
+                const right = document.createElement("div");
+                right.className = "level-right";
+
+                const rightItem = document.createElement("div");
+                rightItem.className = "level-item"; 
+
+                const button = document.createElement("button");
+                button.className = "button is-primary is-medium is-light";
+                button.innerText = "View Hikes";
+                button.id = `${th.id}`;
+                button.addEventListener("click", () => {console.log("Get hikes was clicked")});
+
+                rightItem.appendChild(button);
+                right.appendChild(rightItem);
+                level.appendChild(right)
+            };
+            
+            const content = document.createElement("p");
+            content.innerHTML = `<strong>Location:</strong> ${th.location}<br>
+                                <strong>Fees:</strong> ${th.fees}<br>
+                                <strong>Amentities:</strong> ${th.amenities}<br>
+                                <strong>Number of Hikes:</strong> ${th.hikes_count}`
+
+            child.appendChild(level);
+            child.appendChild(content);
+            parent.appendChild(child);
+            ancestor.appendChild(parent);
+            
+            main.appendChild(ancestor)
+        })
     }
 
 
--- hikers-app-frontend/src/trailhead.js
+++ hikers-app-frontend/src/trailhead.js
@@ -1,10 +1,11 @@
 class Trailhead {
-    constructor(name, location, amenities, fees, hikes) {
+    constructor(name, location, amenities, fees, hikes, id) {
         this.name = name;
         this.location = location;
         this.amenities = amenities;
         this.fees = fees;
         this.hikes_count = hikes.length;
+        this.id = id;
 
         AppMain.trailheads.push(this);
     }
