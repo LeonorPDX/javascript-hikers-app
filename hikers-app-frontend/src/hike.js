a5c7cd2aa925469015a45d4e2b3443012493901a
LeonorPDX <leonoracolbert@gmail.com>
2020-08-26 14:16:15 -0700

    Debugging scope of hike fetch and render. Switched from object to array for hikes, because passing the trailhead id as a parameter in the argument was causing it to be trigger instantly and threw off the AJAX

--- hikers-app-frontend/src/hike.js
+++ hikers-app-frontend/src/hike.js
@@ -1,5 +1,5 @@
 class Hike {
-    constructor(name, difficulty, distance, elevationGain, hikeType, description, imgUrl, th_id) {
+    constructor(name, difficulty, distance, elevationGain, hikeType, description, imgUrl, id, trailheadId) {
         this.name = name;
         this.distance = distance;
         this.difficulty = difficulty;
@@ -7,16 +7,13 @@ class Hike {
         this.hikeType = hikeType;
         this.description = description;
         this.imgUrl = imgUrl;
+        this.id = id;
+        this.trailheadId = trailheadId;
 
-        const keys = Object.keys(Hike.all);
-        if (keys.includes(`${th_id}`)) {
-            Hike.all[th_id].push(this)
-        } else {
-            Hike.all[th_id] = [this]
-        }
+        Hike.all.push(this)
     }
 
-    static all = {} // Access an array of hikes by a key (trailhead ID). On create, add hike to object. No need to make another fetch if there is a key for the desired Trailhead ID already in the AppMain hikes object
+    static all = [] // Access an array of hikes by a key (trailhead ID). On create, add hike to object. No need to make another fetch if there is a key for the desired Trailhead ID already in the AppMain hikes object
 
     static getHikes(event) {
         const th_id = event.target.id;
@@ -25,14 +22,32 @@ class Hike {
             .then(resp => resp.json())
             .then(data => {
                 data.forEach(h => {
-                    new Hike(h.name, h.difficulty, h.distance, h.elevation_gain, h.hike_type, h.description, h.image_url, h.trailhead_id)
+                    new Hike(h.name, h.difficulty, h.distance, h.elevation_gain, h.hike_type, h.description, h.image_url, h.id, h.trailhead_id)
                 })
             })
             .then(Hike.renderHikes)
             .catch(err => alert(err));
+        
     }
 
     static renderHikes() {
-        console.log("Render hikes function was triggered.")
+        console.log("Render hikes function was triggered.");
+        const trailhead_id = Hike.all[0].trailheadId;
+        const th = Trailhead.all.find(e => e.id === trailhead_id);
+        const main = document.getElementById("main");
+        main.innerHTML = ""; // Clear all content from the 'main' section, so no duplicate data if Find Hikes is clicked multiple times
+
+        const headline = document.createElement("div");
+        headline.className = "content";
+        const headlineText = document.createElement("h1");
+        headlineText.className = "title";
+        headlineText.innerText = `All Hikes at ${th.name}`;
+        headline.appendChild(headlineText);
+        main.appendChild(headline);
+
+        const hikes = Hike.all;
+        hikes.forEach(h => {
+            console.log(`${h.name}`)
+        })
     }
 }
