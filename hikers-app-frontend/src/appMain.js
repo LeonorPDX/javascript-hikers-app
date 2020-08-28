b4de82887edf8020ecde3dcfe1f2579657e1064c
LeonorPDX <leonoracolbert@gmail.com>
2020-08-28 14:56:22 -0700

    Started adding a couple form fields and worked on Bulma's styling

--- hikers-app-frontend/src/appMain.js
+++ hikers-app-frontend/src/appMain.js
@@ -4,12 +4,14 @@ class AppMain {
     bindEventListeners() {
         const findHike = document.getElementById("find-hike-btn");
         const addHikeBtn = document.getElementById("add-hike-btn");
-        const hikeFormContainer = document.querySelector(".form-container");
+        const hikeFormContainer = document.getElementById("form-container");
         findHike.addEventListener("click", Trailhead.renderTrailheads);
         addHikeBtn.addEventListener("click", () => {
             this.addHike = !this.addHike;
             if (this.addHike) {
-              hikeFormContainer.style.display = "block";        
+              hikeFormContainer.style.display = "block";
+              // Add event listener for the form
+              // Populate the trailhead options in the form with the Trailhead.all array        
             } else {
               hikeFormContainer.style.display = "none";
             }
