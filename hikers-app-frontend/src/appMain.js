943b6bfb0dfb4309a3972025a137a97e802b1c28
LeonorPDX <leonoracolbert@gmail.com>
2020-08-31 09:25:18 -0700

    Moved the submit functionality to a new fn in AppMain so the 'add hike' event listener is just displaying and populating the form, or hiding the form

--- hikers-app-frontend/src/appMain.js
+++ hikers-app-frontend/src/appMain.js
@@ -10,7 +10,7 @@ class AppMain {
             this.addHike = !this.addHike;
             if (this.addHike) {
               hikeFormContainer.style.display = "block";
-              addHikeBtn.innerText = "Hide hike form"
+              addHikeBtn.innerText = "Hide Hike Form"
 
               const select = document.getElementById("trailhead-select");
               Trailhead.all.forEach(th => {
@@ -20,31 +20,33 @@ class AppMain {
                 select.appendChild(option)
               });
 
-              document.querySelector('.add-hike-form').addEventListener("submit", function(event) {
-                event.preventDefault();
+              document.querySelector('.add-hike-form').addEventListener("submit", () => this.submitForm(event)); // 'this' is the instance of the AppMain class, and passing the event
 
-                const hikeAttr = [];
-
-                hikeAttr.push(document.getElementById("new-hike-name").value);
-                hikeAttr.push(document.getElementById("trailhead-select").value);
-                hikeAttr.push(document.getElementById("new-hike-difficulty").value);
-                hikeAttr.push(document.getElementById("new-hike-distance").value);
-                hikeAttr.push(document.getElementById("new-hike-elevation").value);
-                hikeAttr.push(document.getElementById("new-hike-type").value);
-                hikeAttr.push(document.getElementById("new-hike-img").value);
-                hikeAttr.push(document.getElementById("new-hike-description").value);
-                
-                if (hikeAttr[0] != "") { // Only call config object method if the values are not empty so form reset doesn't trigger empty config objects
-                  Hike.makeConfigObj(hikeAttr)
-                }
-
-                document.querySelector('.add-hike-form').reset();
-                }, false);
             } else {
               hikeFormContainer.style.display = "none";
               addHikeBtn.innerText = "Add a Hike"
             }
           });
+    }
+
+    submitForm(event) {
+      event.preventDefault();
+
+      const hikeAttr = [];
+
+      hikeAttr.push(document.getElementById("new-hike-name").value);
+      hikeAttr.push(document.getElementById("trailhead-select").value);
+      hikeAttr.push(document.getElementById("new-hike-difficulty").value);
+      hikeAttr.push(document.getElementById("new-hike-distance").value);
+      hikeAttr.push(document.getElementById("new-hike-elevation").value);
+      hikeAttr.push(document.getElementById("new-hike-type").value);
+      hikeAttr.push(document.getElementById("new-hike-img").value);
+      hikeAttr.push(document.getElementById("new-hike-description").value);
+      
+      if (hikeAttr[0] != "") { // Only call config object method if the values are not empty so form reset doesn't trigger empty config objects
+        Hike.makeConfigObj(hikeAttr)
+      }
+      document.querySelector('.add-hike-form').reset();
     }
 
 }
